9fc9dad73da49c73ae23536f10ddac1a414a9526
Mikhaylov Konstantin <kamikhaylov@edu.hse.ru>
2024-03-21 11:55:26 +0300

    Added node.js

---
+++ app.js
@@ -0,0 +1,44 @@
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+
+let indexRouter = require('./router/index');
+let usersRouter = require('./router/users');
+
+let app = express();
+
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use("/", express.static("public/html"));
+app.use("/favicon.ico", express.static("public/static/img/favicon.ico"));
+
+app.use("/image", express.static("public/static/img"));
+app.use("/css",express.static("public/styles"));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
